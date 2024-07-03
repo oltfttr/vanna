e6b90fdf75b99c2f09d7b93625ddf72baf813540
altfattero-ext <otto.altfatter@idemtelematics.com>
2024-07-03 10:19:16 +0200

    setup file for importing to other projects

---
+++ src/vanna/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vanna',  # Replace 'vanna' with the actual package name
+    version='0.1.0',
+    packages=find_packages(),
+    # Include other necessary metadata below
+)
